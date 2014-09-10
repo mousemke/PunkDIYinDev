0d92335672a8b251b9a28f159ffc76c2647f1c69
Mouse Braun <mouse@mouse.local>
2014-09-10 19:10:24 +0200

    first commit.  these are things

---
+++ slides/xx-diy-dev.js
@@ -0,0 +1,14 @@
+/**
+ * Do it yourself
+ *
+ * These problems wont solve themselves and are too specific for
+ * broader solutions
+ *
+ * @param  {human, probably}        you
+ *
+ * @return {object}                 solution
+ */
+var diy = function( you, challenge )
+{
+    return you.solveProblems( challenge );
+};
